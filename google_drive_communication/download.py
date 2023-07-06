683153b9db99d25cea4c625a139a3190c85dd078
LYchou <mike.yichou.liu@gmail.com>
2023-07-06 18:00:51 +0800

    delete func : search_files_that_are_in_cloud_folder_but_notin_local_folder()

--- google_drive_communication/download.py
+++ google_drive_communication/download.py
@@ -22,40 +22,6 @@ class Download:
             creds_and_tokem_path=self.creds_and_tokem_path
         )
 
-
-    def search_files_that_are_in_cloud_folder_but_notin_local_folder(self, local_folder_path:str, cloud_folder_id:str) -> list:
-        '''
-        功能:
-            為了不要重複下載內容所需要的功能，尋找本地沒有但雲端有的檔案
-        輸入:
-            :local_folder_path: 下載資料夾路徑
-            :cloud_folder_id: 對應雲端資料夾 id
-
-        輸出:
-            files = [
-                {'id':'XXXX', 'name':'filename.extension'},
-                {'id':'XXXX', 'name':'filename.extension'},
-                ....
-            ]
-        '''
-
-        # 判斷是否有資料夾，沒有的話就創建
-        utils.create_folder(local_folder_path)
-
-        # 取得本地已經有哪些檔案
-        print(f'-- 開始搜尋 {local_folder_path} 本地檔案')
-        existing_file_names = os.listdir(local_folder_path)
-        print(f'-- 完成，共有 {len(existing_file_names)} 個檔案')
-        # 搜尋端檔案
-        print(f'-- 開始搜尋 {local_folder_path} 對應雲端檔案, id={cloud_folder_id}')
-        query = f"'{cloud_folder_id}' in parents"
-        cloud_files = communication_function.query_metadata(service=self.service, fields='id, name', query=query)
-        # 篩選檔案
-        # 找到在雲端不在本地檔案
-        files = [file for file in cloud_files if file['name'] not in existing_file_names]
-
-        return files
-
     def download_file(cloud_file_id:str, save_file:str) -> None:
         '''
         功能:
